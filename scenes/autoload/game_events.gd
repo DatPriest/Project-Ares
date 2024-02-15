5d7c50eeaabbfc40f5b6e9bcd4bb26a880675110
DatPriest <lcwiertnia@web.de>
2024-02-15 16:58:20 +0100

    init

---
+++ scenes/autoload/game_events.gd
@@ -0,0 +1,14 @@
+extends Node
+
+signal experience_vial_collected(number: float)
+signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
+signal enemy_killed(experience_amount: float)
+
+func emit_experience_vial_collected(number: float):
+	experience_vial_collected.emit(number)
+	
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	ability_upgrade_added.emit(upgrade, current_upgrades)
+
+func emit_enemy_killed(experience_amount: float):
+	enemy_killed.emit(experience_amount)
